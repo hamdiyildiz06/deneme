a6f046a795edd932121692b41ef45923cd27bc55
hamdi Yıldız <hamdiyildiz06@gmail.com>
2019-01-24 16:51:32 +0300

    sweetalert ile confirm kullanılarak kayıt silme işlemi gerçekleştirildi

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
 
 
+    $(".remove-btn").click(function () {
+
+        $data_url = $(this).data("url");
+        Swal.fire({
+            title: 'Emin misiniz?',
+            text: "Bu işlemi geri alamayacaksınız!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Sil',
+            cancelButtonText: 'Hayır'
+        }).then((result) => {
+            if (result.value) {
+               window.location.href = $data_url;
+            }
+        });
+    })
 
 
 
