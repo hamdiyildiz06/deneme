96d46f9512967bd637622f2bb5291dfa641613d7
hamdi Yıldız <hamdiyildiz06@gmail.com>
2019-01-24 10:46:30 +0300

    View yapısı tek klasör içerisine atıldı. Viewfolder yapısı eklendi

--- panel/application/controllers/Dashboard.php
+++ panel/application/controllers/Dashboard.php
@@ -3,8 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-	public function index()
+    public $viewFolder = "";
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->viewFolder = "dashboard_v";
+    }
+
+    public function index()
 	{
-		$this->load->view('dashboard_v');
+		$this->load->view("{$this->viewFolder}/index");
 	}
 }
