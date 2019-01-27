c15afbc9bd538a26324519ddd2aa632128755129
hamdi Yıldız <hamdiyildiz06@gmail.com>
2019-01-27 21:49:32 +0300

    css ile ürün fotograflarının tasarımında değişiklikler yapıldı -33

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,10 +1,5 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">
-            Ürünün Fotoğrafları
-        </h4>
-    </div><!-- END column -->
-    <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
                 <form action="../api/dropzone" class="dropzone" data-plugin="dropzone" data-options="{ url: '../api/dropzone'}">
@@ -27,7 +22,7 @@
     <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
-                <table class="table table-bordered table-striped table-hover">
+                <table class="table table-bordered table-striped table-hover pictures_list">
                     <thead>
                     <tr>
                         <th>#id</th>
@@ -38,13 +33,14 @@
                     </tr>
                     </thead>
                     <tbody>
+
                     <tr>
-                        <th>1</th>
-                        <td>
+                        <th class="w100 text-center">1</th>
+                        <td class="w100 text-center">
                             <img width="30" src="http://hamdiyildiz.net/assets/image/sertifika/resim1.jpg" alt="" class="img-responsive">
                         </td>
                         <td>deneme-urun.jpg</td>
-                        <td>
+                        <td class="w100 text-center">
                             <input
                                     data-url="<?= base_url("product/isActiveSetter/"); ?>"
                                     class="isActive"
@@ -54,21 +50,21 @@
                                 <?= (true) ? "checked" : "";  ?>
                             />
                         </td>
-                        <td>
+                        <td class="w100 text-center">
                             <button
                                     data-url="<?= base_url("product/delete/"); ?>"
-                                    class="btn btn-sm btn-danger btn-outline remove-btn">
+                                    class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
                                 <i class="fa fa-trash-o"></i> Sil
                             </button>
                         </td>
                     </tr>
                     <tr>
-                        <th>1</th>
-                        <td>
+                        <th class="w100 text-center">1</th>
+                        <td class="w100 text-center">
                             <img width="30" src="http://hamdiyildiz.net/assets/image/sertifika/resim1.jpg" alt="" class="img-responsive">
                         </td>
                         <td>deneme-urun.jpg</td>
-                        <td>
+                        <td class="w100 text-center">
                             <input
                                     data-url="<?= base_url("product/isActiveSetter/"); ?>"
                                     class="isActive"
@@ -78,21 +74,21 @@
                                 <?= (true) ? "checked" : "";  ?>
                             />
                         </td>
-                        <td>
+                        <td class="w100 text-center">
                             <button
                                     data-url="<?= base_url("product/delete/"); ?>"
-                                    class="btn btn-sm btn-danger btn-outline remove-btn">
+                                    class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
                                 <i class="fa fa-trash-o"></i> Sil
                             </button>
                         </td>
                     </tr>
                     <tr>
-                        <th>1</th>
-                        <td>
+                        <th class="w100 text-center">1</th>
+                        <td class="w100 text-center">
                             <img width="30" src="http://hamdiyildiz.net/assets/image/sertifika/resim1.jpg" alt="" class="img-responsive">
                         </td>
                         <td>deneme-urun.jpg</td>
-                        <td>
+                        <td class="w100 text-center">
                             <input
                                     data-url="<?= base_url("product/isActiveSetter/"); ?>"
                                     class="isActive"
@@ -102,21 +98,21 @@
                                 <?= (true) ? "checked" : "";  ?>
                             />
                         </td>
-                        <td>
+                        <td class="w100 text-center">
                             <button
                                     data-url="<?= base_url("product/delete/"); ?>"
-                                    class="btn btn-sm btn-danger btn-outline remove-btn">
+                                    class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
                                 <i class="fa fa-trash-o"></i> Sil
                             </button>
                         </td>
                     </tr>
                     <tr>
-                        <th>1</th>
-                        <td>
+                        <th class="w100 text-center">1</th>
+                        <td class="w100 text-center">
                             <img width="30" src="http://hamdiyildiz.net/assets/image/sertifika/resim1.jpg" alt="" class="img-responsive">
                         </td>
                         <td>deneme-urun.jpg</td>
-                        <td>
+                        <td class="w100 text-center">
                             <input
                                     data-url="<?= base_url("product/isActiveSetter/"); ?>"
                                     class="isActive"
@@ -126,14 +122,15 @@
                                 <?= (true) ? "checked" : "";  ?>
                             />
                         </td>
-                        <td>
+                        <td class="w100 text-center">
                             <button
                                     data-url="<?= base_url("product/delete/"); ?>"
-                                    class="btn btn-sm btn-danger btn-outline remove-btn">
+                                    class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
                                 <i class="fa fa-trash-o"></i> Sil
                             </button>
                         </td>
                     </tr>
+
                     </tbody>
                 </table>
             </div><!-- .widget-body -->
