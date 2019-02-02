8dfd20eb4f0ecc276f58a3142df6a661a6c0c665
hamdi Yıldız <hamdiyildiz06@gmail.com>
2019-02-02 16:55:37 +0300

    referanslar modülü oluşturuldu -62

--- panel/application/views/references_v/list/content.php
+++ panel/application/views/references_v/list/content.php
@@ -2,8 +2,8 @@
 
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Haber listesi
-            <a href="<?= base_url("news/new_form"); ?>" class="btn pull-right btn-outline btn-primary btn-xs"><i class="fa fa-plus"></i> Yeni Ekle</a>
+            Referans listesi
+            <a href="<?= base_url("references/new_form"); ?>" class="btn pull-right btn-outline btn-primary btn-xs"><i class="fa fa-plus"></i> Yeni Ekle</a>
         </h4>
     </div><!-- END column -->
     <div class="col-md-12">
@@ -11,7 +11,7 @@
             <?php if (empty($items)){ ?>
                 <div class="alert alert-info text-center ">
                     <h5 class="alert-title">Kayıt Bulunamadı</h5>
-                    <p>Burada herhangi bir veri bulunamamaktadır. Eklemek için lütfen <a href="<?= base_url("news/new_form"); ?>">Tıklayınız</a></p>
+                    <p>Burada herhangi bir veri bulunamamaktadır. Eklemek için lütfen <a href="<?= base_url("references/new_form"); ?>">Tıklayınız</a></p>
                 </div>
             <?php }else{ ?>
                 <table class="table table-hover table-bordered table-striped content-container">
@@ -21,43 +21,24 @@
                             <th class="w50">#id</th>
                             <th>Başlık</th>
                             <th>Url</th>
-                            <th>Haber Türü</th>
                             <th>Görsel</th>
                             <th>Durumu</th>
                             <th class="text-center">İşlemler</th>
                         </tr>
                     </thead>
-                    <tbody class="sortable" data-url="<?= base_url("news/rankSetter"); ?>">
+                    <tbody class="sortable" data-url="<?= base_url("references/rankSetter"); ?>">
                 <?php foreach ($items as $item): ?>
                     <tr id="ord-<?= $item->id; ?>">
                         <th class="text-center"><i class="fa fa-reorder"></i></th>
                         <th class="text-center"><?= $item->id; ?></th>
                         <td><?= $item->title; ?></td>
                         <td><?= $item->url; ?></td>
-                        <td><?= $item->news_type; ?></td>
                         <td>
-                                <?php if ($item->news_type == "image"){ ?>
-                                    <img width="75" src="<?= base_url("uploads/{$viewFolder}/{$item->img_url}") ?>"
-                                         alt=""
-                                         class="img-rounded">
-                                <?php }else if ($item->news_type == "video"){ ?>
-                                    <iframe
-                                            width="75"
-                                            src="<?= $item->video_url; ?>"
-                                            frameborder="0"
-                                            allow="accelerometer;
-                                            autoplay;
-                                            encrypted-media;
-                                            gyroscope;
-                                            picture-in-picture"
-                                            allowfullscreen>
-
-                                    </iframe>
-                                <?php } ?>
+                            <img width="75" src="<?= base_url("uploads/{$viewFolder}/{$item->img_url}") ?>" alt="" class="img-rounded">
                         </td>
                         <td>
                             <input
-                                    data-url="<?= base_url("news/isActiveSetter/{$item->id}"); ?>"
+                                    data-url="<?= base_url("references/isActiveSetter/{$item->id}"); ?>"
                                     class="isActive"
                                     type="checkbox"
                                     data-switchery
@@ -67,11 +48,11 @@
                         </td>
                         <td class="text-center">
                             <button
-                                    data-url="<?= base_url("news/delete/{$item->id}"); ?>"
+                                    data-url="<?= base_url("references/delete/{$item->id}"); ?>"
                                     class="btn btn-sm btn-danger btn-outline remove-btn">
                                     <i class="fa fa-trash-o"></i> Sil
                             </button>
-                            <a href="<?= base_url("news/update_form/{$item->id}"); ?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+                            <a href="<?= base_url("references/update_form/{$item->id}"); ?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
