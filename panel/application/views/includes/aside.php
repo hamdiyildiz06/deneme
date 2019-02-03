0eba1d7d026654403af8e0ed8727e18a1433061b
hamdi Yıldız <hamdiyildiz06@gmail.com>
2019-02-03 12:10:41 +0300

    markalar->brands bölümü listelemesi yapıldı -68

--- panel/application/views/includes/aside.php
+++ panel/application/views/includes/aside.php
@@ -3,12 +3,12 @@
         <div class="media">
             <div class="media-left">
                 <div class="avatar avatar-md avatar-circle">
-                    <a href="javascript:void(0)"><img class="img-responsive" src="<?= base_url("assets"); ?>/assets/images/221.jpg" alt="avatar"/></a>
+                    <a href="<?= base_url(); ?>"><img class="img-responsive" src="<?= base_url("assets"); ?>/assets/images/221.jpg" alt="avatar"/></a>
                 </div><!-- .avatar -->
             </div>
             <div class="media-body">
                 <div class="foldable">
-                    <h5><a href="javascript:void(0)" class="username">John Doe</a></h5>
+                    <h5><a href="<?= base_url(); ?>" class="username">John Doe</a></h5>
                     <ul>
                         <li class="dropdown">
                             <a href="javascript:void(0)" class="dropdown-toggle usertitle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -116,7 +116,7 @@
                 </li>
 
                 <li>
-                    <a href="javascript:void(0)">
+                    <a href="<?= base_url("brands"); ?>">
                         <i class="menu-icon zmdi zmdi-puzzle-piece zmdi-hc-lg"></i>
                         <span class="menu-text">Markalar</span>
                     </a>
